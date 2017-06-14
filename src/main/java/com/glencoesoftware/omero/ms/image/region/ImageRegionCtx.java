8a09ec815b290a0dad5bfb4d05c1c751e8445a0e
Emil Rozbicki <emil@glencoesoftware.com>
2017-06-14 15:54:53 +0200

    Make ImageRegionCtx jackson compatible

--- src/main/java/com/glencoesoftware/omero/ms/image/region/ImageRegionCtx.java
+++ src/main/java/com/glencoesoftware/omero/ms/image/region/ImageRegionCtx.java
@@ -19,14 +19,9 @@
 package com.glencoesoftware.omero.ms.image.region;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
 import org.slf4j.LoggerFactory;
-
-import io.vertx.core.MultiMap;
-import io.vertx.core.json.JsonArray;
+import io.vertx.core.MultiMap;;
 
 public class ImageRegionCtx {
 
@@ -34,42 +29,47 @@ public class ImageRegionCtx {
             LoggerFactory.getLogger(ImageRegionCtx.class);
 
     /** Image Id*/
-    private Long imageId;
+    public Long imageId;
 
     /** z - index */
-    private Integer z;
+    public Integer z;
 
     /** t - index */
-    private Integer t;
+    public Integer t;
 
     /** tile descriptor (Region) */
-    private String tile;
+    public List<Integer> tile;
+
+    /** resolution to read */
+    public Integer resolution;
 
     /** tile descriptor (Region) */
-    private String region;
+    public List<Integer> region;
 
     /** channel settings - handled at Verticle level*/
-    private String c;
+    public List<Integer> channels;
+    public List<Integer[]> windows;
+    public List<String> colors;
 
     /** Color mode (g == grey scale; c == rgb) */
-    private String m;
+    public String m;
 
     /** Maps. <b>Not</b> handled at the moment. Supported from 5.3.0 */
-    private String maps;
+    public String maps;
 
     /** Compression quality */
-    private String compressionQuality;
+    public Float compressionQuality;
 
     /** Projection 'intmax' OR 'intmax|5:25'
      * NOT handled at the moment - does not look like it's supported
      * for renderImageRegion: https://github.com/openmicroscopy/openmicroscopy/blob/be40a59300bb73a22b72eac00dd24b2aa54e4768/components/tools/OmeroPy/src/omero/gateway/__init__.py#L8758
      * vs. renderImage: https://github.com/openmicroscopy/openmicroscopy/blob/be40a59300bb73a22b72eac00dd24b2aa54e4768/components/tools/OmeroPy/src/omero/gateway/__init__.py#L8837
      * */
-    private String projection;
+    public String projection;
 
     /** Inverted Axis
      *  NOT handled at the moment - no use cases*/
-    private Boolean invertedAxis;
+    public Boolean invertedAxis;
 
     /**
      * Default constructor.
@@ -80,25 +80,49 @@ public class ImageRegionCtx {
         imageId = Long.parseLong(params.get("imageId"));
         z = Integer.parseInt(params.get("z"));
         t = Integer.parseInt(params.get("t"));
-        tile = params.get("tile");
-        region = params.get("region");
-        c = params.get("c");
-        m = params.get("m");
-        compressionQuality = params.get("q");
+        getTileFromString(params.get("tile"));
+        getRegionFromString(params.get("region"));
+        getChannelInfoFromString(params.get("c"));
+        getColorModelFromString(params.get("m"));
+        getCompressionQualityFromString(params.get("q"));
+        getInvertedAxisFromString(params.get("ia"));
         projection = params.get("p");
         maps = params.get("maps");
-        invertedAxis = Boolean.parseBoolean(params.get("ia"));
-        log.debug("Image:{}, z: {}, t: {}, tile: {}, c: {}, m: {}",
-                this.imageId, this.z, this.t,
-                this.tile, this.c, this.m);
+        log.debug("{}, z: {}, t: {}, tile: {}, c: [{}, {}, {}], m: {}",
+                imageId, z, t, tile, channels, windows, colors, m);
     }
 
-    private Map<String, Object> formatChannelInfo(String channelsFromRequest) {
-        Map<String, Object> channels = new HashMap<String, Object>();
-        String[] channelArray = channelsFromRequest.split(",", -1);
-        List<Integer> activeChannels = new ArrayList<Integer>();
-        List<String> colors = new ArrayList<String>();
-        List<Integer[]> windows = new ArrayList<Integer[]>();
+    private void getTileFromString(String tileString) {
+        if (tileString == null) {
+            return;
+        }
+        String[] tileArray = tileString.split(",", -1);
+        tile = new ArrayList<Integer>();
+        tile.add(Integer.parseInt(tileArray[1]));
+        tile.add(Integer.parseInt(tileArray[2]));
+        resolution = Integer.parseInt(tileArray[0]);
+    }
+
+    private void getRegionFromString(String regionString) {
+        if (regionString == null) {
+            return;
+        }
+        String[] regionSplit = regionString.split(",", -1);
+        region = new ArrayList<Integer>();
+        region.add(Integer.parseInt(regionSplit[0]));
+        region.add(Integer.parseInt(regionSplit[1]));
+        region.add(Integer.parseInt(regionSplit[2]));
+        region.add(Integer.parseInt(regionSplit[3]));
+    }
+
+    private void getChannelInfoFromString(String channelInfo) {
+        if (channelInfo == null) {
+            return;
+        }
+        String[] channelArray = channelInfo.split(",", -1);
+        channels = new ArrayList<Integer>();
+        windows = new ArrayList<Integer[]>();
+        colors = new ArrayList<String>();
         for (String channel : channelArray) {
             // chan  1|12:1386r$0000FF
             // temp ['1', '12:1386r$0000FF']
@@ -114,7 +138,7 @@ public class ImageRegionCtx {
                 active = split[0];
                 color = split[1];
             }
-            activeChannels.add(Integer.parseInt(active));
+            channels.add(Integer.parseInt(active));
             if (temp.length > 1) {
                 if (temp[1].indexOf("$") >= 0) {
                     window = temp[1].split("\\$")[0];
@@ -131,69 +155,31 @@ public class ImageRegionCtx {
             log.debug("Adding channel: {}, color: {}, window: {}",
                     active, color, window);
         }
-        channels.put("active", activeChannels);
-        channels.put("colors", colors);
-        channels.put("windows", windows);
-        return channels;
     }
 
-    public Map<String, Object> getImageRegionRaw() {
-        Map<String, Object> data = new HashMap<String, Object>();
-        data.put("imageId", imageId);
-        data.put("t", t);
-        data.put("z", z);
-        data.put("tile", tile);
-        data.put("region", region);
-        data.put("c", c);
-        data.put("m", m);
-        data.put("compressionQuality", compressionQuality);
-        data.put("invertedAxis", invertedAxis);
-        data.put("projection", projection);
-        data.put("maps", maps);
-        return data;
-    }
-
-    public Map<String, Object> getImageRegionFormatted() {
-        Map<String, Object> data = new HashMap<String, Object>();
-        data.put("imageId", imageId);
-        data.put("t", t);
-        data.put("z", z);
-        data.put("tile", null);
-        data.put("resolution", null);
-        data.put("region", null);
-        if (this.tile != null) {
-            String[] tileArray = tile.split(",", -1);
-            JsonArray tileCoor = new JsonArray();
-            tileCoor.add(Integer.parseInt(tileArray[1]));
-            tileCoor.add(Integer.parseInt(tileArray[2]));
-            data.put("tile", tileCoor);
-            data.put("resolution", Integer.parseInt(tileArray[0]));
-        } else if (this.region != null) {
-            String[] regionSplit = region.split(",", -1);
-            JsonArray regionCoor = new JsonArray();
-            regionCoor.add(Integer.parseInt(regionSplit[0]));
-            regionCoor.add(Integer.parseInt(regionSplit[1]));
-            regionCoor.add(Integer.parseInt(regionSplit[2]));
-            regionCoor.add(Integer.parseInt(regionSplit[3]));
-            data.put("region", regionCoor);
-        }
-        data.put("channelInfo", formatChannelInfo(c));
-        String model = null;
-        if (m != null && m.equals("g")) {
-            model = "greyscale";
-        } else if (m != null && m.equals("c")) {
-            model = "rgb";
-        }
-        data.put("m", model);
-        if (compressionQuality != null) {
-            data.put("compressionQuality",
-                     Float.parseFloat(compressionQuality));
+    private void getColorModelFromString(String colorModel) {
+        if (colorModel != null && colorModel.equals("g")) {
+            m = "greyscale";
+        } else if (colorModel != null && colorModel.equals("c")) {
+            m = "rgb";
         } else {
-            data.put("compressionQuality", compressionQuality);
+            m = null;
         }
-        data.put("invertedAxis", invertedAxis);
-        data.put("projection", projection);
-        data.put("maps", maps);
-        return data;
+    }
+
+    private void getCompressionQualityFromString(String quality) {
+        if (quality == null) {
+            compressionQuality = null;
+            return;
+        }
+        compressionQuality = Float.parseFloat(quality);
+    }
+
+    private void getInvertedAxisFromString(String iaString) {
+        if (iaString == null) {
+            invertedAxis = null;
+            return;
+        }
+        invertedAxis = Boolean.parseBoolean(iaString);
     }
 }
