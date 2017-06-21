b9b37c6b7ed81b82ec7eabb21a76a7c9f3077440
Chris Allan <callan@glencoesoftware.com>
2017-06-21 13:37:27 +0100

    Large functionality update
    This commit includes several functionality and architectural updates
    surrounding image rendering.  In particular it bring the actual
    rendering itself in process by including the rendering engine and
    associated components in the dependency tree and establishing a
    Spring application context.  This now means the microservice acts
    like a mini-OMERO or OMERO-light exposing the OMERO server
    functionality over HTTP rather than Ice.
    
    Image and image region endpoints are now exposed, allowing for
    complete replacement of the OMERO.web image viewer utilised URLs.
    JPEG, PNG, and TIFF resulting content types are supported for
    all these endpoints via the `type=<type>` parameter with the latter
    two being losslessly compressed end-to-end.
    
    Mask image rendering has also been added using the indexed colour
    space.  This results in a significant improvement in resultant
    mask image size (anywhere from 2x+), memory utilisation, and
    performance (somewhere between 5-10x depending on hardware) over
    the current OMERO.web implementation.
    
    Single image region performance is marginally better than
    experienced previously, largely due to session joining and
    Bio-Formats memoizer overhead, but overall concurrency is **much**
    higher.  Somewhere between 2-5x depending on the hardware in use
    and number of cores available.
    
    As the image data is accessed directly from the filesystem the
    microservice will now scale almost completely linearly in the
    horizontal direction.

---
+++ src/main/java/com/glencoesoftware/omero/ms/image/region/ShapeMaskCtx.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2017 Glencoe Software, Inc. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package com.glencoesoftware.omero.ms.image.region;
+
+import org.slf4j.LoggerFactory;
+
+import com.glencoesoftware.omero.ms.core.OmeroRequestCtx;
+
+import io.vertx.core.MultiMap;
+
+public class ShapeMaskCtx extends OmeroRequestCtx {
+
+    private static final org.slf4j.Logger log =
+            LoggerFactory.getLogger(ShapeMaskCtx.class);
+
+    /** Shape Id */
+    public Long shapeId;
+
+    /** Display color */ 
+    public String color;
+
+    /**
+     * Constructor for jackson to decode the object from string
+     */
+    ShapeMaskCtx() {};
+
+    /**
+     * Default constructor.
+     * @param params {@link io.vertx.core.http.HttpServerRequest} parameters
+     * required for rendering an shape mask.
+     * @param omeroSessionKey OMERO session key.
+     */
+    ShapeMaskCtx(MultiMap params, String omeroSessionKey) {
+        this.omeroSessionKey = omeroSessionKey;
+        shapeId = Long.parseLong(params.get("shapeId"));
+        color = params.get("color");
+
+        log.debug("Shape:{}, color: {}", shapeId, color);
+    }
+
+}
