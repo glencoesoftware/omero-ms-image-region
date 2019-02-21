c98e2846a2dafd85fbf8cf498d252f4cdea17540
Chris Allan <callan@glencoesoftware.com>
2019-02-21 08:55:35 +0000

    Remove unused imports

--- src/main/java/com/glencoesoftware/omero/ms/image/region/ImageRegionVerticle.java
+++ src/main/java/com/glencoesoftware/omero/ms/image/region/ImageRegionVerticle.java
@@ -22,9 +22,7 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.ObjectInputStream;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 import org.perf4j.StopWatch;
