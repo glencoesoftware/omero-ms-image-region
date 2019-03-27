f203225ff672a4e8e64a3e73d12f8a12d4e78d3d
Chris MacLeod <ckm@glencoesoftware.com>
2019-03-26 21:27:02 -0400

    debuggin travis

--- docker/build.sh
+++ docker/build.sh
@@ -12,10 +12,12 @@ IMAGE=${MS_NAME}:${TAG}
 # you cannot add/copy src files from outside of docker in the Dockerfile
 # so we do it here and make a nice tar.gz while we are at it
 # this may not work on windows builds
-cp -n ../build/distributions/${MS_NAME}-*.tar .
+echo "Copying dist tar into docker build dir $(PWD)"
+cp -vn ../build/distributions/${MS_NAME}-*.tar .
 gzip ${MS_NAME}-*.tar
 
 docker build -t ${IMAGE} .
 
+echo "Cleaning up dist tar"
 rm -f ${MS_NAME}-*.tar.gz
 
