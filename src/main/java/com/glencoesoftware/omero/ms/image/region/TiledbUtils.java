2af832c4e8d79cd653c3caf903cff1c77103537c
Kevin Kozlowski <kevin@glencoesoftware.com>
2020-10-21 08:45:06 -0600

    Cleanup, add unit tests, fix bugs

--- src/main/java/com/glencoesoftware/omero/ms/image/region/TiledbUtils.java
+++ src/main/java/com/glencoesoftware/omero/ms/image/region/TiledbUtils.java
@@ -33,84 +33,6 @@ public class TiledbUtils {
     private static final org.slf4j.Logger log =
         LoggerFactory.getLogger(ShapeMaskRequestHandler.class);
 
-    public static byte getMaxByte(byte[] bytes) {
-        if (bytes.length == 0) {
-            throw new IllegalArgumentException("Cannot get max of empty array");
-        } else {
-            byte max = bytes[0];
-            for(int i = 1; i < bytes.length; i++) {
-                if (bytes[i] > max) {
-                    max = bytes[i];
-                }
-            }
-            return max;
-        }
-    }
-
-    public static int getMaxUnsignedByte(byte[] bytes) {
-        if (bytes.length == 0) {
-            throw new IllegalArgumentException("Cannot get max of empty array");
-        } else {
-            int max = bytes[0] & 0xff;
-            for(int i = 1; i < bytes.length; i++) {
-                int val = bytes[i] & 0xff;
-                if (val > max) {
-                    max = val;
-                }
-            }
-            return max;
-        }
-    }
-
-    public static int getMinUnsignedByte(byte[] bytes) {
-        if (bytes.length == 0) {
-            throw new IllegalArgumentException("Cannot get max of empty array");
-        } else {
-            int max = bytes[0] & 0xff;
-            for(int i = 1; i < bytes.length; i++) {
-                int val = bytes[i] & 0xff;
-                if (val > max) {
-                    max = val;
-                }
-            }
-            return max;
-        }
-    }
-
-    public static int getMaxUnsignedShort(byte[] bytes) {
-        if (bytes.length == 0) {
-            throw new IllegalArgumentException("Cannot get max of empty array");
-        } else {
-            ByteBuffer bbuf = ByteBuffer.wrap(bytes);
-            ShortBuffer sbuf = bbuf.asShortBuffer();
-            int max = sbuf.get() & 0xffff;
-            while(sbuf.hasRemaining()) {
-                int val = sbuf.get() & 0xffff;
-                if(val > max) {
-                    max = val;
-                }
-            }
-            return max;
-        }
-    }
-
-    public static int getMinUnsignedShort(byte[] bytes) {
-        if (bytes.length == 0) {
-            throw new IllegalArgumentException("Cannot get max of empty array");
-        } else {
-            ByteBuffer bbuf = ByteBuffer.wrap(bytes);
-            ShortBuffer sbuf = bbuf.asShortBuffer();
-            int min = sbuf.get() & 0xffff;
-            while(sbuf.hasRemaining()) {
-                int val = sbuf.get() & 0xffff;
-                if(val < min) {
-                    min = val;
-                }
-            }
-            return min;
-        }
-    }
-
     public static long[] getMinMax(ByteBuffer buf, Datatype type) {
         if (!buf.hasRemaining()) {
             throw new IllegalArgumentException("Cannot get max of empty buffer");
@@ -127,10 +49,11 @@ public class TiledbUtils {
                 return new long[] {min, max};
             }
             case TILEDB_INT8: {
-                byte min = (byte) 0;
-                byte max = (byte) 0;
+                long max = buf.get();
+                long min = max;
                 while (buf.hasRemaining()) {
                     byte next = buf.get();
+                    log.info(Byte.toString(next));
                     min = next < min ? next : min;
                     max = next > max ? next : max;
                 }
@@ -147,8 +70,8 @@ public class TiledbUtils {
                 return new long[] {min, max};
             }
             case TILEDB_INT16: {
-                short min = (short) 0;
-                short max = (short) 0;
+                long max = buf.getShort();
+                long min = max;
                 while (buf.hasRemaining()) {
                     short next = buf.getShort();
                     min = next < min ? next : min;
@@ -157,18 +80,18 @@ public class TiledbUtils {
                 return new long[] {(long) min, (long) max};
             }
             case TILEDB_UINT32:{
-                long max = buf.getInt() & 0xffffffff;
+                long max = buf.getInt() & 0xffffffffl;
                 long min = max;
                 while(buf.hasRemaining()) {
-                    long val = buf.getInt() & 0xffffffff;
+                    long val = buf.getInt() & 0xffffffffl;
                     min = val < min ? val : min;
                     max = val > max ? val : max;
                 }
                 return new long[] {min, max};
             }
             case TILEDB_INT32: {
-                int min = 0;
-                int max = 0;
+                long max = buf.getInt();
+                long min = max;
                 while (buf.hasRemaining()) {
                     int next = buf.getInt();
                     min = next < min ? next : min;
@@ -311,7 +234,7 @@ public class TiledbUtils {
             throw new IllegalArgumentException("Number of dimensions must be 5. Actual was: "
                     + Integer.toString(num_dims));
         }
-        long[] subarrayDomain = TiledbUtils.getSubarrayDomainFromString(subarrayString);
+        long[] subarrayDomain = getSubarrayDomainFromString(subarrayString);
         log.info(Arrays.toString(subarrayDomain));
 
         int capacity = 1;
