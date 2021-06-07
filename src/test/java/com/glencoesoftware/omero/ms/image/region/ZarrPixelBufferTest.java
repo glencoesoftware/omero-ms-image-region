d781e699cd3d4228f0fd19ef36ca91d103b1656f
Kevin Kozlowski <kevin@glencoesoftware.com>
2021-06-07 09:23:25 -0600

    Make tmp test files in

--- src/test/java/com/glencoesoftware/omero/ms/image/region/ZarrPixelBufferTest.java
+++ src/test/java/com/glencoesoftware/omero/ms/image/region/ZarrPixelBufferTest.java
@@ -33,7 +33,7 @@ import zipkin2.reporter.Reporter;
 public class ZarrPixelBufferTest {
 
     @Rule
-    public TemporaryFolder tmpDir = new TemporaryFolder();
+    public TemporaryFolder tmpDir = new TemporaryFolder(new File("/tmp"));
 
     /**
      * Run the bioformats2raw main method and check for success or failure.
