dbb298d9b166f8909999cc198b59506cbd2af9c3
Kevin Kozlowski <kevin@glencoesoftware.com>
2021-11-02 09:09:21 -0600

    Add pathviewer endpoint

--- src/main/java/com/glencoesoftware/omero/ms/image/region/ImageRegionMicroserviceVerticle.java
+++ src/main/java/com/glencoesoftware/omero/ms/image/region/ImageRegionMicroserviceVerticle.java
@@ -335,6 +335,8 @@ public class ImageRegionMicroserviceVerticle extends AbstractVerticle {
         // ImageData request handlers
         router.get("/webgateway/imgData/:imageId").handler(this::getImageData);
         router.get("/webgateway/imgData/:imageId/:keys").handler(this::getImageData);
+        router.get("/pathviewer/imgData/:imageId").handler(this::getImageData);
+        router.get("/pathviewer/imgData/:imageId/:keys").handler(this::getImageData);
 
         // ShapeMask request handlers
         router.get(
