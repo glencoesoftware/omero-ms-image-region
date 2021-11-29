e5c504344a0927b4e0b489fa9e0dff2bfd601482
Chris Allan <callan@glencoesoftware.com>
2021-11-29 11:23:24 +0000

    Thorough Zarr pixel buffer implementation

--- src/test/java/com/glencoesoftware/omero/ms/image/region/ZarrPixelBufferTest.java
+++ src/test/java/com/glencoesoftware/omero/ms/image/region/ZarrPixelBufferTest.java
@@ -28,6 +28,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -35,9 +37,14 @@ import org.junit.Test;
 import com.bc.zarr.ZarrArray;
 
 import brave.Tracing;
+import loci.formats.FormatTools;
+import loci.formats.in.FakeReader;
 import ome.io.nio.DimensionsOutOfBoundsException;
 import ome.model.core.Pixels;
+import ome.model.enums.DimensionOrder;
 import ome.util.PixelData;
+import ucar.ma2.Array;
+import ucar.ma2.DataType;
 import ucar.ma2.InvalidRangeException;
 import zipkin2.reporter.Reporter;
 
@@ -175,6 +182,117 @@ public class ZarrPixelBufferTest extends AbstractZarrPixelBufferTest {
         }
     }
 
+    private Array asArray(byte[] storage, int[] shape) {
+        // XXX: Is not data type agnostic, expects signed 32-bit integer pixels
+        int size = IntStream.of(shape).reduce(1, Math::multiplyExact);
+        int[] asInt = new int[size];
+        ByteBuffer.wrap(storage).asIntBuffer().get(asInt);
+        return Array.factory(DataType.INT, shape, asInt);
+    }
+
+    private byte[] getStack(
+            byte[] timepoint, int c, int sizeC, int sizeZ, int sizeX,
+            int sizeY) {
+        // XXX: Is not data type agnostic, expects signed 32-bit integer pixels
+        int bytesPerPixel = 4;
+        int[] shape = new int[] {sizeC, sizeZ, sizeY, sizeX};
+        int size = IntStream.of(new int[] {sizeZ, sizeY, sizeX, bytesPerPixel})
+                .reduce(1, Math::multiplyExact);
+        Array array = asArray(timepoint, shape).slice(0, c);
+        byte[] asBytes = new byte[size];
+        ByteBuffer.wrap(asBytes).asIntBuffer()
+                .put((int[]) array.copyTo1DJavaArray());
+        return asBytes;
+    }
+
+    private byte[] getPlane(
+            byte[] stack, int z, int sizeZ, int sizeX, int sizeY) {
+        // XXX: Is not data type agnostic, expects signed 32-bit integer pixels
+        int bytesPerPixel = 4;
+        int[] shape = new int[] {sizeZ, sizeY, sizeX};
+        int size = IntStream.of(new int[] {sizeY, sizeX, bytesPerPixel})
+                .reduce(1, Math::multiplyExact);
+        Array array = asArray(stack, shape).slice(0, z);
+        byte[] asBytes = new byte[size];
+        ByteBuffer.wrap(asBytes).asIntBuffer()
+                .put((int[]) array.copyTo1DJavaArray());
+        return asBytes;
+    }
+
+    private byte[] getCol(byte[] plane, int x, int sizeX, int sizeY) {
+        // XXX: Is not data type agnostic, expects signed 32-bit integer pixels
+        int bytesPerPixel = 4;
+        int[] shape = new int[] {sizeY, sizeX};
+        int size = IntStream.of(new int[] {sizeY, bytesPerPixel})
+                .reduce(1, Math::multiplyExact);
+        Array array = asArray(plane, shape).slice(1, x);
+        byte[] asBytes = new byte[size];
+        ByteBuffer.wrap(asBytes).asIntBuffer()
+                .put((int[]) array.copyTo1DJavaArray());
+        return asBytes;
+    }
+
+    @Test
+    public void testGetTimepointStackPlaneRowCol()
+            throws IOException, InvalidRangeException {
+        int sizeT = 2;
+        int sizeC = 3;
+        int sizeZ = 4;
+        int sizeY = 1024;
+        int sizeX = 2048;
+        int resolutions = 1;
+        Pixels pixels = new Pixels(
+                null, null, sizeX, sizeY, sizeZ, sizeC, sizeT, "", null);
+        Path output = writeTestZarr(
+                sizeT, sizeC, sizeZ, sizeY, sizeX, "int32", resolutions);
+        Tracing.newBuilder().spanReporter(Reporter.NOOP).build();
+        try (ZarrPixelBuffer zpbuf =
+                new ZarrPixelBuffer(pixels, output.resolve("0"), 2048)) {
+            for (int t = 0; t < sizeT; t++) {
+                // Assert timepoint
+                byte[] timepoint = zpbuf.getTimepoint(t).getData().array();
+                for (int c = 0; c < sizeC; c++) {
+                    // Assert stack
+                    byte[] stack = zpbuf.getStack(c, t).getData().array();
+                    byte[] stackFromTimepoint =
+                            getStack(timepoint, c, sizeC, sizeZ, sizeX, sizeY);
+                    Assert.assertArrayEquals(stack, stackFromTimepoint);
+                    for (int z = 0; z < sizeZ; z++) {
+                        // Assert plane
+                        byte[] plane =
+                            zpbuf.getPlane(z, c, t).getData().array();
+                        byte[] planeFromStack =
+                                getPlane(stack, z, sizeZ, sizeX, sizeY);
+                        Assert.assertArrayEquals(plane, planeFromStack);
+                        int[] seriesPlaneNumberZCT =
+                            FakeReader.readSpecialPixels(
+                                    plane, zpbuf.getPixelsType(), false);
+                        int planeNumber = FormatTools.getIndex(
+                                DimensionOrder.VALUE_XYZCT,
+                                sizeZ, sizeC, sizeT, sizeZ * sizeC * sizeT,
+                                z, c, t);
+                        Assert.assertArrayEquals(
+                                new int[] {0, planeNumber, z, c, t},
+                                seriesPlaneNumberZCT);
+                        // Assert row
+                        int y = sizeY / 2;
+                        int rowSize = zpbuf.getRowSize();
+                        int rowOffset = y * rowSize;
+                        byte[] row = zpbuf.getRow(y, z, c, t).getData().array();
+                        byte[] rowExpected = Arrays.copyOfRange(
+                                plane, rowOffset, rowOffset + rowSize);
+                        Assert.assertArrayEquals(rowExpected, row);
+                        // Assert column
+                        int x = sizeX / 2;
+                        byte[] col = zpbuf.getCol(x, z, c, t).getData().array();
+                        byte[] colExpected = getCol(plane, x, sizeX, sizeY);
+                        Assert.assertArrayEquals(colExpected, col);
+                    }
+                }
+            }
+        }
+    }
+
     @Test(expected = DimensionsOutOfBoundsException.class)
     public void testGetTileLargerThanImage()
             throws IOException, InvalidRangeException {
@@ -304,6 +422,7 @@ public class ZarrPixelBufferTest extends AbstractZarrPixelBufferTest {
         }
     }
 
+    @Test
     public void testGetTileSize() throws IOException {
         int sizeT = 1;
         int sizeC = 1;
@@ -323,4 +442,89 @@ public class ZarrPixelBufferTest extends AbstractZarrPixelBufferTest {
         }
     }
 
+    @Test
+    public void testUint16() throws IOException {
+        int sizeT = 1;
+        int sizeC = 1;
+        int sizeZ = 1;
+        int sizeY = 2048;
+        int sizeX = 2048;
+        int resolutions = 3;
+        int bytesPerPixel = 2;
+        Pixels pixels = new Pixels(
+                null, null, sizeX, sizeY, sizeZ, sizeC, sizeT, "", null);
+        Path output = writeTestZarr(
+                sizeT, sizeC, sizeZ, sizeY, sizeX, "uint16", resolutions);
+        try (ZarrPixelBuffer zpbuf =
+                new ZarrPixelBuffer(pixels, output.resolve("0"), 1024)) {
+            Assert.assertEquals(FormatTools.UINT16, zpbuf.getPixelsType());
+            Assert.assertEquals(false, zpbuf.isSigned());
+            Assert.assertEquals(false, zpbuf.isFloat());
+            Assert.assertEquals(bytesPerPixel, zpbuf.getByteWidth());
+        }
+    }
+
+    @Test
+    public void testFloat() throws IOException {
+        int sizeT = 1;
+        int sizeC = 1;
+        int sizeZ = 1;
+        int sizeY = 2048;
+        int sizeX = 2048;
+        int resolutions = 3;
+        int bytesPerPixel = 4;
+        Pixels pixels = new Pixels(
+                null, null, sizeX, sizeY, sizeZ, sizeC, sizeT, "", null);
+        Path output = writeTestZarr(
+                sizeT, sizeC, sizeZ, sizeY, sizeX, "float", resolutions);
+        try (ZarrPixelBuffer zpbuf =
+                new ZarrPixelBuffer(pixels, output.resolve("0"), 1024)) {
+            Assert.assertEquals(FormatTools.FLOAT, zpbuf.getPixelsType());
+            Assert.assertEquals(true, zpbuf.isSigned());
+            Assert.assertEquals(true, zpbuf.isFloat());
+            Assert.assertEquals(bytesPerPixel, zpbuf.getByteWidth());
+        }
+    }
+
+    @Test
+    public void testSizes() throws IOException {
+        int sizeT = 4;
+        int sizeC = 2;
+        int sizeZ = 3;
+        int sizeY = 1024;
+        int sizeX = 2048;
+        int resolutions = 3;
+        int bytesPerPixel = 2;
+        Pixels pixels = new Pixels(
+                null, null, sizeX, sizeY, sizeZ, sizeC, sizeT, "", null);
+        Path output = writeTestZarr(
+                sizeT, sizeC, sizeZ, sizeY, sizeX, "uint16", resolutions);
+        try (ZarrPixelBuffer zpbuf =
+                new ZarrPixelBuffer(pixels, output.resolve("0"), 1024)) {
+            // Plane size
+            Assert.assertEquals(
+                    sizeX * sizeY * bytesPerPixel,
+                    zpbuf.getPlaneSize().longValue());
+            // Stack size
+            Assert.assertEquals(
+                    sizeZ * sizeX * sizeY * bytesPerPixel,
+                    zpbuf.getStackSize().longValue());
+            // Timepoint size
+            Assert.assertEquals(
+                    sizeC * sizeZ * sizeX * sizeY * bytesPerPixel,
+                    zpbuf.getTimepointSize().longValue());
+            // Total size
+            Assert.assertEquals(
+                    sizeT * sizeC * sizeZ * sizeX * sizeY * bytesPerPixel,
+                    zpbuf.getTotalSize().longValue());
+            // Column size
+            Assert.assertEquals(
+                    sizeY * bytesPerPixel,
+                    zpbuf.getColSize().longValue());
+            // Row size
+            Assert.assertEquals(
+                    sizeX * bytesPerPixel,
+                    zpbuf.getRowSize().longValue());
+        }
+    }
 }
